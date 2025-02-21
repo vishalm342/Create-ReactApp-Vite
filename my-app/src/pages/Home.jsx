1f2edfd22cf3abf415090f43946e5d5202b81df2
vishalm342 <vishalm8656@gmail.com>
2025-02-21 10:53:05 +0530

    Home page created

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hi, I am Vishal</h1>
+      <h2>I am a Full-Stack Developer</h2>
+      <p>Contact: vihalm8656@gmail.com</p>
+    </div>
+  );
+};
+
+export default Home;
